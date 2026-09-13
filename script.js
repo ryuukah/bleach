461472c02377275d2ebc717d2a9d7e20173c4d8b
ryuka <domodachiiwa@gmail.com>
2026-09-13 14:14:33 -0300

    script.js

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
 const image = document.getElementById("musicImage");
 const audio = document.getElementById("audio");
+const cd = document.getElementById("cd");
 
 image.addEventListener("click", () => {
   if (audio.paused) {
     audio.play();
+    cd.classList.add("spinning");
   } else {
     audio.pause();
+    cd.classList.remove("spinning");
   }
+});
+
+/* Stop the spinning when the song naturally ends */
+audio.addEventListener("ended", () => {
+  cd.classList.remove("spinning");
 });
